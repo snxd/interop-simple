9274784065c19791d15f25befe124189ccabef7e
Nathan Moinvaziri <nathan@solidstatenetworks.com>
2024-08-16 14:05:59 -0700

    Use newer ES6 javascript and interop calls.

--- js/Simple.js
+++ js/Simple.js
@@ -2,119 +2,116 @@
  *  @class Simple Object
  *  @brief
  */
+import {EventEmitter} from "events";
 
 import {interop} from "../direct/Host";
 
-// Node JS support
-var EventEmitter;
-if (typeof (require) !== "undefined") {
-  if (typeof (EventEmitter) == "undefined") {
-    EventEmitter = require("events");
+class Simple extends EventEmitter {
+  constructor(instanceId) {
+    super();
+    this.instanceId = instanceId;
+    this.refCount = 1;
+  }
+
+  addRef() {
+    this.refCount++;
+  }
+  release() {
+    if (--this.refCount === 0) {
+      this.emit("release");
+
+      return interop.releaseInstance(this.instanceId);
+    }
+  }
+  releaseAsync() {
+    if (--this.refCount === 0) {
+      this.emit("release");
+
+      return interop.releaseInstanceAsync(this.instanceId);
+    }
+    return Promise.resolve();
+  }
+  invoke(method, methodArgs) {
+    return interop.invoke(this.instanceId, method, methodArgs);
+  }
+  invokeAsync(method, methodArgs) {
+    if (this.refCount === 0) throw Error("Unable to call 'invokeAsync' of null reference");
+    const self = this, releaseRef = () => self.release();
+    this.addRef();
+    return interop.invokeAsync(this.instanceId, method, methodArgs).then(releaseRef, releaseRef);
+  }
+  invokeMemoize(method, methodArgs) {
+    return interop.invokeMemoize(this.instanceId, method, methodArgs);
+  }
+  invokeMemoizeAsync(method, methodArgs) {
+    if (this.refCount === 0) throw Error("Unable to call 'invokeMemoizeAsync' of null reference");
+    const self = this, releaseRef = () => self.release();
+    this.addRef();
+    return interop.invokeMemoizeAsync(this.instanceId, method, methodArgs).then(releaseRef, releaseRef);
+  }
+  /**
+   * Gets an integer value
+   * @type int
+   */
+  getIntProperty() {
+    return this.invoke("getIntProperty");
+  }
+  /**
+   * Sets an integer value
+   */
+  setIntProperty(value) {
+    return this.invoke("setIntProperty", {value});
+  }
+  /**
+   * Gets a double value
+   * @type float
+   */
+  getFloatProperty() {
+    return this.invoke("getFloatProperty");
+  }
+  /**
+   * Sets a double value
+   */
+  setFloatProperty(value) {
+    return this.invoke("setFloatProperty", {value});
+  }
+  /**
+   * Gets a boolean value
+   * @type bool
+   */
+  getBooleanProperty() {
+    return this.invoke("getBooleanProperty");
+  }
+  /**
+   * Sets a boolean value
+   */
+  setBooleanProperty(value) {
+    return this.invoke("setBooleanProperty", {value});
+  }
+  /**
+   * Gets a string value
+   * @type string
+   */
+  getStringProperty() {
+    return this.invoke("getStringProperty");
+  }
+  /**
+   * Sets a string value
+   */
+  setStringProperty(value) {
+    return this.invoke("setStringProperty", {value});
+  }
+  /**
+   * Starts a request for a value
+   */
+  startValueRequest() {
+    return this.invoke("startValueRequest");
   }
 }
 
-function Simple(instanceId) {
-  this.instanceId = instanceId;
-}
-
-Simple.prototype = Object.create(EventEmitter.prototype, {
-  constructor: {value: Simple, enumerable: false, writable: true, configurable: true}
-});
-
-Simple.prototype.release = function() {
-  this.emit("finalize");
-  this.releaseInstance();
-};
-Simple.prototype.invoke = function(methodBinding) {
-  return interop.invoke(this.instanceId, methodBinding);
-};
-Simple.prototype.releaseInstance = function() {
-  interop.releaseInstance(this.instanceId);
-};
-
-/**
- * Gets an integer value
- * @type int
- */
-Simple.prototype.getIntProperty = function() {
-  return this.invoke({
-    "method": "getIntProperty"
-  });
-};
-/**
- * Sets an integer value
- */
-Simple.prototype.setIntProperty = function(value) {
-  return this.invoke({
-    "method": "setIntProperty",
-    "value": value
-  });
-};
-/**
- * Gets a double value
- * @type float
- */
-Simple.prototype.getFloatProperty = function() {
-  return this.invoke({
-    "method": "getFloatProperty"
-  });
-};
-/**
- * Sets a double value
- */
-Simple.prototype.setFloatProperty = function(value) {
-  return this.invoke({
-    "method": "setFloatProperty",
-    "value": value
-  });
-};
-/**
- * Gets a boolean value
- * @type bool
- */
-Simple.prototype.getBooleanProperty = function() {
-  return this.invoke({
-    "method": "getBooleanProperty"
-  });
-};
-/**
- * Sets a boolean value
- */
-Simple.prototype.setBooleanProperty = function(value) {
-  return this.invoke({
-    "method": "setBooleanProperty",
-    "value": value
-  });
-};
-/**
- * Gets a string value
- * @type string
- */
-Simple.prototype.getStringProperty = function() {
-  return this.invoke({
-    "method": "getStringProperty"
-  });
-};
-/**
- * Sets a string value
- */
-Simple.prototype.setStringProperty = function(value) {
-  return this.invoke({
-    "method": "setStringProperty",
-    "value": value
-  });
-};
-/**
- * Starts a request for a value
- */
-Simple.prototype.startValueRequest = function() {
-  return this.invoke({
-    "method": "startValueRequest"
-  });
-};
-
-var createSimple = function(instanceId) {
-  return interop.createInstance("SSN.Simple", Simple, instanceId);
-};
-
-export {createSimple};
+export const createSimple = (instanceId) =>
+  interop.createInstance("SSN.Simple", Simple, instanceId);
+export const createAnalyticsAsync = (instanceId) =>
+  interop.createInstanceAsync("SSN.Simple", Simple, instanceId);
+export const createSimpleCopy = (obj) =>
+  new Simple(interop.getInstanceId(obj));
